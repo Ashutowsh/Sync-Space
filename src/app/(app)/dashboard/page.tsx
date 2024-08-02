d1ad20471f3461a61b2efb040e66784f3e2cf59f
Ashutosh <Ashutosh@LAPTOP-N00785I6>
2024-08-02 15:12:24 +0530

    Project Initialted

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function Dashbord() {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  )
+}
+
+export default Dashbord
